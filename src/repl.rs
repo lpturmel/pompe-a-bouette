b4ddef06648011dc8cbda062e79363ecf09b613e
Louis-Philippe Turmel <turmlo@2020spaces.com>
2023-10-08 23:24:47 -0400

    Added basic commands

--- src/repl.rs
+++ src/repl.rs
@@ -1,8 +1,43 @@
 use crate::lexer::Lexer;
 use crate::token::TokenType;
 use std::io::{self, BufRead, BufReader, Stdin, Write};
+use std::str::FromStr;
 
 const PROMPT: &str = ">> ";
+
+#[derive(Debug)]
+enum Commands {
+    Exit,
+    Help,
+}
+
+impl Commands {
+    fn handle(&self) {
+        match self {
+            Self::Exit => {
+                println!("Bye!");
+                std::process::exit(0);
+            }
+            Self::Help => {
+                println!("Commands:");
+                println!("exit: Exit the REPL");
+                println!("help: Print this help message");
+            }
+        }
+    }
+}
+
+impl FromStr for Commands {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "exit" => Ok(Self::Exit),
+            "help" => Ok(Self::Help),
+            _ => Err(format!("Unknown command: {}", s)),
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Repl {
@@ -36,6 +71,12 @@ impl Repl {
             io::stdout().flush().unwrap();
             let mut input = String::new();
             self.reader.read_line(&mut input).unwrap();
+
+            // check to see if the input is a command
+            if let Ok(command) = input.trim().parse::<Commands>() {
+                command.handle();
+                continue;
+            }
 
             let mut lexer = Lexer::new(&input);
             loop {
